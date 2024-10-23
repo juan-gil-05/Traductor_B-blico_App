5a1f15600fc6c93769e52a0552683ac96a9f529f
Juan Esteban Gil Martínez <gilj06@hotmail.com>
2024-10-23 19:09:49 +0200

    creación de la base de datos en firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:traductor_biblico/firebase_options.dart';
 import 'package:traductor_biblico/pages/splash_page.dart';
 
-void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
